3ca80f3866c5d3ee53ffa46407f99050009a7f29
Varun Gohil <vg296@cornell.edu>
2022-01-12 12:34:36 -0500

    Binded URL params to add courses

--- enrollment-app/src/app/dashboard/dashboard-search/dashboard-search.component.ts
+++ enrollment-app/src/app/dashboard/dashboard-search/dashboard-search.component.ts
@@ -16,9 +16,42 @@ export class DashboardSearchComponent {
   @Output() addClassEvent = new EventEmitter<boolean>();
   values = ''
   searchResults: Class[] = [];
+  wrkAddQuery:boolean = false;
+
+  
   constructor(public dialog: MatDialog, private router: Router, private route: ActivatedRoute) { 
-    console.log("INSIDE SEARCHHHH constructor");
-  }
+      console.log("INSIDE SEARCHHHH constructor");
+      // let username:string = "";
+      // let code:string = "";
+      this.route.queryParams.subscribe(params => {
+        this.wrkAddQuery = params['wrkaddquery'];
+      });
+      if(this.wrkAddQuery)
+      {
+        console.log("Calling open dialog")
+        this.openDialog(this.searchResults[0]);
+      }
+      // console.log(this.wrkAddQuery + " " + this.courseCode)
+      // if(this.wrkQuery){
+      //   this.classclient.getClassList("SP21").asObservable().pipe().subscribe(val =>  {
+      //     console.log("val = " + val)
+      //     this.wrkClasses = val;
+      //   })
+      //   console.log("wrkQuery is true")
+      //   console.log("classes length = " + this.wrkClasses.length)
+      //   this.searchResults = this.wrkClasses.filter(
+      //     class_ => ( class_.getTitle().includes(this.courseCode) || class_.getCode().includes(this.courseCode) || class_.getTitle().toLowerCase().includes(this.courseCode.toLowerCase()) || class_.getCode().toLowerCase().includes(this.courseCode.toLowerCase()) ));
+      //   console.log("search Results [0] = " + this.searchResults[0]);
+      //   this.openDialog(this.searchResults[0]);
+      // }
+    }
+
+  // ngOnInit() :void {
+  //   if(this.wrkQuery)
+  //   {
+
+  //   }
+  // }
 
   openDialog(class_: Class): void {
     const dialogRef = this.dialog.open(DialogSearch, {
@@ -46,7 +79,7 @@ export class DashboardSearchComponent {
 
   visitClass(classNum: any) {
     this.router.navigate(
-      ['class/SP21/ECE/'+classNum], { relativeTo: this.route },
+      ['class/ECE/'+classNum], { relativeTo: this.route },
     );
   }
 }
@@ -63,16 +96,70 @@ export class DialogSearch {
   selectedLec = ""; 
   selectedLab = ""; 
   selectedDis = ""; 
+  wrkAddQuery:boolean = false;
+  user: string = "";
+  courseCode: string = "";
+  lec:string = "";
+  lab:string = "";
+  dis:string = "";
 
   constructor(
     public dialogRef: MatDialogRef<DialogSearch>,
-    @Inject(MAT_DIALOG_DATA) public data: Class, private cartClient: CartClientService, private student: StudentStateService) {
-      this.lecs = data.getSectionsList().filter(section => section.getTitle().includes("LEC"));
-      this.labs = data.getSectionsList().filter(section => section.getTitle().includes("LAB"));
-      this.discs = data.getSectionsList().filter(section => section.getTitle().includes("DIS"));
-      if (this.lecs.length > 0) this.selectedLec = this.lecs[0].getTitle(); 
-      if (this.labs.length > 0) this.selectedLab = this.labs[0].getTitle(); 
-      if (this.discs.length > 0) this.selectedDis = this.discs[0].getTitle(); 
+    @Inject(MAT_DIALOG_DATA) public data: Class, private cartClient: CartClientService, private student: StudentStateService, private route: ActivatedRoute) {
+      this.route.queryParams.subscribe(params => {
+        this.wrkAddQuery = params['wrkaddquery'];
+        this.courseCode = "ECE " + params['code'];
+        this.lec = "LEC 00" + params['lec']
+        this.dis = "DIS 20" + params['dis']
+        this.lab = "LAB 40" + params['lab']
+        this.user = params['user']
+      });
+      console.log(this.wrkAddQuery + " " + this.courseCode + " " + this.user)
+      if(this.wrkAddQuery)
+      {
+        console.log("Inside wrkAddquery dialog")
+        var secslist:section[] = [];
+        // console.log("this.secs = " + this.secs)
+        // for(var sec in this.secs)
+        // {
+        //   console.log(sec)
+        if(this.lec != "LEC 00")
+        {
+          var newsection:section = new section();
+          newsection.setSec(this.lec);
+          secslist[0] = newsection
+        }
+        if(this.lab != "LAB 40")
+        {
+          var newsection:section = new section();
+          newsection.setSec(this.lab);
+          secslist[1] = newsection
+        }
+        if(this.dis != "DIS 20")
+        {
+          var newsection:section = new section();
+          newsection.setSec(this.dis);
+          secslist[2] = newsection
+        }
+          // secslist[i] = newsection;
+          // i++;
+        // }
+        console.log(secslist);
+        console.log("Adding class");
+        this.cartClient.addClass(this.user, this.courseCode, secslist );
+      }
+      else
+      {
+        console.log("Inside Dialog Search")
+
+        this.lecs = data.getSectionsList().filter(section => section.getTitle().includes("LEC"));
+        this.labs = data.getSectionsList().filter(section => section.getTitle().includes("LAB"));
+        this.discs = data.getSectionsList().filter(section => section.getTitle().includes("DIS"));
+        if (this.lecs.length > 0) this.selectedLec = this.lecs[0].getTitle(); 
+        if (this.labs.length > 0) this.selectedLab = this.labs[0].getTitle(); 
+        if (this.discs.length > 0) this.selectedDis = this.discs[0].getTitle(); 
+      }
+      
     }
 
   onNoClick(): void {
@@ -84,23 +171,28 @@ export class DialogSearch {
     //if( this.selectedLec != '') this.lecSuccess = this.cartClient.addClass(this.student.getUsername(), this.data.getCode(), this.selectedLec );
     let i=0
     if( this.selectedLec != '') {
+      console.log(this.selectedLec)
       var newsection:section = new section();
       newsection.setSec(this.selectedLec);
       sectionList[i] = newsection;
       i++;
     } 
     if( this.selectedLab != '') {
+      console.log(this.selectedLab)
       var newsection:section = new section();
       newsection.setSec(this.selectedLab);
       sectionList[i] = newsection;
       i++;
     } 
     if( this.selectedDis != '') {
+      console.log(this.selectedDis)
       var newsection:section = new section();
       newsection.setSec(this.selectedDis);
       sectionList[i] = newsection;
       i++;
     } 
+    console.log("Section List = " + sectionList)
+
     this.cartClient.addClass(this.student.getUsername(), this.data.getCode(),sectionList );
     // alert main component about the dialog result. 
     this.dialogRef.close(this.selectedLec != ''||this.selectedLec != ''||this.selectedLec != '');
